4ee4fe00cc5ccd536fd562c88bcd7829792cfa3b
klishey <46532628+klishey@users.noreply.github.com>
2024-04-19 10:54:41 +0800

    Body recolor

--- src/component/Fab.js
+++ src/component/Fab.js
@@ -1,7 +1,7 @@
 export default function Fab() {
     return (
         <div className="position-fixed user-fab d-block d-sm-none">
-            <a className="bg-info rounded-5 btn-circle shadow" href="#">
+            <a className="bg-info rounded-5 btn-circle shadow-lg" href="#">
                 <i className="bi bi-rocket-takeoff-fill text-white"></i>
             </a>
         </div>
